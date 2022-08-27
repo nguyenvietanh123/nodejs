08a4f01a87801215bde50263272470cbdc7095cc
nguyenvietanh123 <anhnvth2203034@fpt.edu.vn>
2022-08-27 09:32:14 +0700

    update

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ app.listen(port,function() {
     console.log("Sever is running...");
 })
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 const mysql = require('mysql');
 const conn = mysql.createConnection({
     host: 'db4free.net',
